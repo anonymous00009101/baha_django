0d270195498f1d7c8e6e2609af3bfdad9575c4dc
Shakal <anonymous00009101@gmail.com>
2025-05-09 15:35:26 +0500

    commit

--- diction_ai/urls.py
+++ diction_ai/urls.py
@@ -1,22 +1,8 @@
-"""
-URL configuration for diction_ai project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +10,7 @@ urlpatterns = [
     path('', lambda request: redirect('login')),  # Перенаправление на страницу входа
     path('', include('speech.urls')),  # Подключение маршрутов приложения speech
 ]
+
+# Обработка медиафайлов
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- speech/models.py
+++ speech/models.py
@@ -10,3 +10,12 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class AudioRecord(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='audio_records')
+    file = models.FileField(upload_to='audio/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=[('pending', 'Pending'), ('processed', 'Processed')], default='pending')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.file.name}"
